69fed62bc61eebe0ccd6694a4c27099fc06bb2b0
osayl12 <hamedosayl@gmail.com>
2026-02-12 15:25:51 +0200

    code improvment

--- site/menugame/menu.js
+++ site/menugame/menu.js
@@ -1,80 +1,74 @@
-
 function startGame() {
-    window.location.href = '../ingame/game.html?score=0'; 
+  window.location.href = "../ingame/game.html?score=0";
 }
 
 function resetGame() {
+  localStorage.setItem("bestScore", "0");
+  window.location.href = "menu.html";
+}
 
-    window.location.href = 'menu.html'; 
-  localStorage.setItem('bestScore', '0');
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const currentScore = urlParams.get("score");
 
-  window.location.href = 'menu.html'; 
-
-  bestScoreDisplay.textContent = `Best Score: 0`;}
-
-document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const currentScore = urlParams.get('score');
-
-    const scoreDisplay = document.getElementById('scoreDisplay');
-    if (currentScore !== null) {
-        scoreDisplay.textContent = `Your Last Score is: ${currentScore}`;
-    }
+  const scoreDisplay = document.getElementById("scoreDisplay");
+  if (currentScore !== null) {
+    scoreDisplay.textContent = `Your Last Score is: ${currentScore}`;
+  }
 });
-const bestScore = localStorage.getItem('bestScore');
+const bestScore = localStorage.getItem("bestScore");
 
-const bestScoreDisplay = document.querySelector('.best-score');
+const bestScoreDisplay = document.querySelector(".best-score");
 
 bestScoreDisplay.textContent = `Best Score: ${bestScore || 0}`;
 
 function toggleAudio() {
-    var audio = document.getElementById("backgroundMusic");
-    var playButton = document.getElementById("customPlayButton");
+  var audio = document.getElementById("backgroundMusic");
+  var playButton = document.getElementById("customPlayButton");
 
-    if (audio.paused) {
-        audio.play();
-        playButton.innerHTML = '<i class="fas fa-pause"></i>'; 
-    } else {
-        audio.pause();
-        playButton.innerHTML = '<i class="fas fa-play"></i>'; 
-    }
+  if (audio.paused) {
+    audio.play();
+    playButton.innerHTML = '<i class="fas fa-pause"></i>';
+  } else {
+    audio.pause();
+    playButton.innerHTML = '<i class="fas fa-play"></i>';
+  }
 }
 function setVolume() {
-    var audio = document.getElementById("backgroundMusic");
-    var volumeSlider = document.getElementById("volumeSlider");
-    audio.volume = volumeSlider.value;
+  var audio = document.getElementById("backgroundMusic");
+  var volumeSlider = document.getElementById("volumeSlider");
+  audio.volume = volumeSlider.value;
 }
 let currentTime = 0;
 let totalDuration = 0;
 
 function updateMusicDuration() {
-    const audio = document.getElementById('backgroundMusic');
-    const durationElement = document.querySelector('.music-duration');
+  const audio = document.getElementById("backgroundMusic");
+  const durationElement = document.querySelector(".music-duration");
 
-    audio.addEventListener('timeupdate', function () {
-        currentTime = audio.currentTime;
-        totalDuration = audio.duration;
+  audio.addEventListener("timeupdate", function () {
+    currentTime = audio.currentTime;
+    totalDuration = audio.duration;
 
-        const currentMinutes = Math.floor(currentTime / 60);
-        const currentSeconds = Math.floor(currentTime % 60);
-        const totalMinutes = Math.floor(totalDuration / 60);
-        const totalSeconds = Math.floor(totalDuration % 60);
+    const currentMinutes = Math.floor(currentTime / 60);
+    const currentSeconds = Math.floor(currentTime % 60);
+    const totalMinutes = Math.floor(totalDuration / 60);
+    const totalSeconds = Math.floor(totalDuration % 60);
 
-        const formattedCurrentTime = `${currentMinutes}:${currentSeconds < 10 ? '0' : ''}${currentSeconds}`;
-        const formattedTotalDuration = `${totalMinutes}:${totalSeconds < 10 ? '0' : ''}${totalSeconds}`;
+    const formattedCurrentTime = `${currentMinutes}:${currentSeconds < 10 ? "0" : ""}${currentSeconds}`;
+    const formattedTotalDuration = `${totalMinutes}:${totalSeconds < 10 ? "0" : ""}${totalSeconds}`;
 
-        durationElement.textContent = `Duration: ${formattedCurrentTime} / ${formattedTotalDuration}`;
-    });
+    durationElement.textContent = `Duration: ${formattedCurrentTime} / ${formattedTotalDuration}`;
+  });
 }
 
 function changeMusic(trackUrl) {
-    const audio = document.getElementById('backgroundMusic');
-    const musicSource = document.getElementById('musicSource');
+  const audio = document.getElementById("backgroundMusic");
+  const musicSource = document.getElementById("musicSource");
 
-    musicSource.src = trackUrl;
-    audio.load();
-    audio.play();
+  musicSource.src = trackUrl;
+  audio.load();
+  audio.play();
 
-    updateMusicDuration(); 
+  updateMusicDuration();
 }
-
